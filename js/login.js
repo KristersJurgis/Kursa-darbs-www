82961c180ad9cd29b98bb028de9347f0d4addfa3
KristersJurgis <kristerssjurgiss@gmail.com>
2025-10-30 16:25:26 +0200

    sync local changes

--- js/login.js
+++ js/login.js
@@ -13,4 +13,59 @@ loginLink.addEventListener('click', ()=> {
 
 btnPopup.addEventListener('click', ()=> {
     wrapper.classList.add('active');
+<<<<<<< HEAD
 });
+=======
+});
+
+document.getElementById('register').addEventListener('Submit', function(event) {
+    event.preventDefault();
+
+    let valid = true;
+        // username validation
+    const name = document.getElementById('name').value;
+    if (name === '' || !/^[a-zA-Z ] + $/.test(name)) {
+        document.getElementById('nameError').textContent = 'Please enter a valid name.';
+        valid = false;
+
+    }
+    else {
+        document.getElementById('nameError').textContent = ''; 
+    }
+
+    // email validation
+
+    const email = document.getElementById('eMail').value;
+    if (email === '' || !/^[^\s@] + @[^\s@] +\.[^s@]+$/.test(email)) {
+        document.getElementById('emailError').textContent = 'Please enter a valid email.';
+        valid = false;
+    }
+    else {
+        document.getElementById('emailError').textContent = '';
+    }
+
+    // password validation
+
+    const password = document.getElementById('password').value;
+    const confirmPassword = document.getElementById('confirmPassword').value;
+    const passwordError = document.getElementById('password-error').value;
+    const confirmPasswordError = document.getElementById('confirmPasswordError').value;
+    if (password.length < 8 ||  !/[A-Z]/.test(password) || !/[a-z]/.test(password) || !/[0-9]/.test(password)){
+        passwordError.textContent = 'Password must be atleast 8 characters long, and include uppercase, lowercase and a number.';
+        valid = false;
+    }
+    else {
+        passwordError.textContent = ' ';
+    // checks if password matches confirm password
+    } 
+    if (password !== confirmPassword) {
+        confirmPasswordError.textContent = 'Passwords do not match.';
+        valid = false;
+    }
+    else {
+        confirmPasswordError.textContent = ' '; 
+    }
+
+
+})
+>>>>>>> 0a9eeb2 (sync local changes)
