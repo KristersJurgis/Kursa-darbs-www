f3a1deed323ebe014ae6d51c481ad2c9a3814380
KristersJurgis <kristerssjurgiss@gmail.com>
2025-11-05 21:27:15 +0200

    aaa

--- js/Flashcards.js
+++ js/Flashcards.js
@@ -1,7 +1,27 @@
 const easyBtn = document.getElementById("easyBtn");
 const mediumBtn = document.getElementById("mediumBtn");
-const hardBtn = document.getElementById("hardBtn");
+const hardBtn = document.getElementById("hardBtn"); 
+const diffbtns = document.getElementById("diffbtns");
+const message = document.getElementById("message");
+const heading = document.getElementById("h1");
+const displayflashcard = document.getElementById("displayflashcard");
+const didntknowBtn = document.getElementById("DidntknowBtn");
+const skipBtn = document.getElementById("SkipBtn");
+const knewBtn = document.getElementById("KnewBtn");
 
+//  Show difficulty selection buttons, and put outs what difficluty you chose.
+function difficultySelected(selected){
+  displayflashcard.style.display = "grid";
+  heading.style.display = "none";
+  diffbtns.style.display = "none";
+  message.textContent = `You selected: ${selected}`;
+}
+
+easyBtn.addEventListener("click", () => difficultySelected("Easy"));
+mediumBtn.addEventListener("click", () => difficultySelected("Medium"));
+hardBtn.addEventListener("click", ()  => difficultySelected("Hard"));
+
+// Gets the selected language from the url  
 function getQueryParam(param) {
   const UrlLanParams = new URLSearchParams(window.location.search);
   return UrlLanParams.get(param);
@@ -9,4 +29,6 @@ function getQueryParam(param) {
 
 const selectedLanguage = getQueryParam('language') || 'japanese';
 
-fetch("")
+fetch("flashcardDb.json")
+
+let score = 0;
--- js/main.js
+++ js/main.js
@@ -1,11 +1,14 @@
+//For browsers under 800px, opens the sidebar.
 function showsidebar() {
     const sidebar = document.querySelector('.sidebar');
     sidebar.style.display = 'flex';
 }
+//For browsers under 800px, closes the sidebar.
 function closesidebar() {
     const sidebar = document.querySelector('.sidebar');
     sidebar.style.display = 'None';
 }
+//Gets the nav.html file, so it can be loaded in other html files just by a div
 fetch('../nav.html')
   .then(response => response.text())
   .then(data => {
@@ -13,6 +16,7 @@ fetch('../nav.html')
   })
   .catch(err => console.error('Failed to load nav:', err));
 
+//Gets the footer.html file, so it can be easiely loaded in other html files just by a div.
 fetch('../footer.html')
   .then(response => response.text())
   .then(data => {
