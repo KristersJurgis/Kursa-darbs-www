a807d7fd9d65b31e489e77fecf3d5764821e4e88
KristersJurgis <Kristerssjurgiss@gmail.com>
2025-11-05 23:12:20 +0200

    nuuu

--- js/Flashcards.js
+++ js/Flashcards.js
@@ -8,13 +8,30 @@ const displayflashcard = document.getElementById("displayflashcard");
 const didntknowBtn = document.getElementById("DidntknowBtn");
 const skipBtn = document.getElementById("SkipBtn");
 const knewBtn = document.getElementById("KnewBtn");
+const btngrid = document.getElementById("btngrid")
 
 //  Show difficulty selection buttons, and put outs what difficluty you chose.
 function difficultySelected(selected){
+  btngrid.style.display = "grid";
   displayflashcard.style.display = "grid";
   heading.style.display = "none";
   diffbtns.style.display = "none";
   message.textContent = `You selected: ${selected}`;
+
+  // Filters cards by language and difficultyt
+  flashcards = allFlashcards.filter(card =>
+    card.difficulty.toLowerCase() === selected.toLowerCase() &&
+    card.language.toLowerCase() === selectedLanguage.toLowerCase()
+  );
+
+  if(flashcards.lenght === 0) {
+    displayflashcard.textContent = "No flashcards found";
+    btngrid.display.style = "none";
+    return;
+  };
+
+  currentIndex = 0;
+  showFlashcard(flashcards[currentIndex]);
 }
 
 easyBtn.addEventListener("click", () => difficultySelected("Easy"));
@@ -26,9 +43,34 @@ function getQueryParam(param) {
   const UrlLanParams = new URLSearchParams(window.location.search);
   return UrlLanParams.get(param);
 };
-
 const selectedLanguage = getQueryParam('language') || 'japanese';
 
+//Loads flashcards from flashcardDb.json
+let allFlashcards = [];
 fetch("flashcardDb.json")
+.then(res => res.json())
+.then(data => {
+  allFlashcards = data;
+})
+.catch(err =>{
+  console.error("Failed to load flashcards", err);
+});
 
-let score = 0;
+let correctscore = 0;
+let skipscore = 0;
+let didntknowscore = 0;
+
+knewBtn.addEventListener("click", () => {
+  knewCount++;
+  console.log("Knew count:", knewCount);
+});
+
+skipBtn.addEventListener("click", () => {
+  skipCount++;
+  console.log("Skipped count: ", skipCount);
+});
+
+didntknowBtn.addEventListener("click", () => {
+  didntknowCount++;
+  console.log("Didn't know count: ", didntknowCount);
+});
